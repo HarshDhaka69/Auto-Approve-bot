ef009372630b67220bf395d4ae6135d08c0a4656
HarshDhaka69 <149968506+HarshDhaka69@users.noreply.github.com>
2024-07-19 11:38:54 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = getenv("API_HASH", "d7f5fb245e4c0b489cba4f7b45bc4173")
     BOT_TOKEN = getenv("BOT_TOKEN", "7273391027:AAFyB5RRXoFpdBEle5hBQPsKnL7wp0_9v_s")
     FSUB = getenv("FSUB", "TestingSuxbot")
-    CHID = int(getenv("CHID", "-1002219153273"))
+    CHID = int(getenv("CHID", ""))
     SUDO = list(map(int, getenv("SUDO", "6288275206").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://itsharshx432:FIdinAchypJomJ8@cluster0.a3fdtex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
