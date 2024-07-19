fe96f2e150a077ba8c92e62ca1295178eb73174a
HarshDhaka69 <149968506+HarshDhaka69@users.noreply.github.com>
2024-07-19 11:50:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,8 +65,12 @@ async def op(_, m :Message):
                 ]
             )
             add_user(m.from_user.id)
-            await m.reply_photo("https://graph.org/file/9d7084b4468f9cf219b5e.jpg", caption="👋 Hey {},/n/n➻ I Can Accept User Join Requests Automatically./n/n➻ I Can Accept All Pending Requests./n/n➻ Add Me To Your Chat And Make Admin With Invite Users Via Link Rights".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
-    
+           await m.reply_photo(
+    "https://graph.org/file/9d7084b4468f9cf219b5e.jpg",
+    caption="👋 Hey {},\n\n➻ I Can Accept User Join Requests Automatically.\n\n➻ I Can Accept All Pending Requests.\n\n➻ Add Me To Your Chat And Make Admin With Invite Users Via Link Rights".format(m.from_user.mention),
+    reply_markup=keyboard
+)
+        
         elif m.chat.type == enums.ChatType.GROUP or enums.ChatType.SUPERGROUP:
             keyboar = InlineKeyboardMarkup(
                 [
