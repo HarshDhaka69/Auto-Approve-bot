cc745b4df75d3be05d9488d74017fda54223e38e
HarshDhaka69 <149968506+HarshDhaka69@users.noreply.github.com>
2024-07-19 11:30:09 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,9 +4,9 @@ class Config:
     API_ID = int(getenv("API_ID", "2092870"))
     API_HASH = getenv("API_HASH", "d7f5fb245e4c0b489cba4f7b45bc4173")
     BOT_TOKEN = getenv("BOT_TOKEN", "7273391027:AAFyB5RRXoFpdBEle5hBQPsKnL7wp0_9v_s")
-    FSUB = getenv("FSUB", "Anshuigroha")
-    CHID = int(getenv("CHID", "-1000112234"))
+    FSUB = getenv("FSUB", "TestingSuxbot")
+    CHID = int(getenv("CHID", "-1002219153273"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://itsharshx432:FIdinAchypJomJ8@cluster0.a3fdtex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
